be44e6822238833bc0e1df06b81045743f576cd1
salvatoreDeploy <liderhenrique@gmail.com>
2023-03-22 06:08:52 -0300

    feat: add componente Link active class

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 import { SignGithub } from "../SignGithub";
+import Link from "next/link";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLInk";
 export function Header() {
+  const {} = useRouter();
+
   return (
     <>
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
           <img src="/images/logoimg.svg" alt="ig.news" />
           <nav>
-            <a className={styles.active} href="#">
-              Home
-            </a>
-            <a href="#">Post</a>
+            <ActiveLink activeClassName={styles.active} href="/">
+              <a>Home</a>
+            </ActiveLink>
+            <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+              <a>Post</a>
+            </ActiveLink>
           </nav>
           <SignGithub />
         </div>
