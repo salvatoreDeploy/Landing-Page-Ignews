7adc47ca21967bb0fe91942977097af5fed83faa
salvatoreDeploy <liderhenrique@gmail.com>
2023-03-23 16:55:45 -0300

    feat: Page Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { getPrismicClient } from "../../services/prismic";
 import * as Prismic from "@prismicio/client";
 import styles from "./styles.module.scss";
 import { RichText } from "prismic-dom";
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -27,11 +28,13 @@ export default function Posts({ posts }: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updateAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
